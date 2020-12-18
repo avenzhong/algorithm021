4bdbc42b1a36d5edd986f98ed17ab5564f2aa96d
钟文坚 <avensnowman@gmail.com>
2020-12-18 12:52:46 +0800

    二叉树的最大深度

--- Week_03/BinaryTreeSolution.java
+++ Week_03/BinaryTreeSolution.java
@@ -1,7 +1,5 @@
-import sun.jvm.hotspot.debugger.Page;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @description
@@ -11,9 +9,9 @@ import java.util.List;
  */
 public class BinaryTreeSolution {
 
-    // 递归
-    private void invertTree(TreeNode root) {
-        if(root == null) {
+    // 递归 翻转二叉树
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
             return null;
         }
         TreeNode left = invertTree(root.left);
@@ -23,4 +21,48 @@ public class BinaryTreeSolution {
         return root;
     }
 
+
+    // 验证二叉搜索树 二叉树的中序遍历
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        // 用来存放前一个节点
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+
+    // 在二叉搜索树计算第k小的值
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                root = stack.pop();
+                root = root.left;
+            }
+            root = stack.pop();
+            if (--k == 0) break;
+            root = root.right;
+        }
+        return root.val;
+    }
+
+    // 二叉树的最大深度
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+ 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
